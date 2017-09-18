d9406537c0f7c9d07a71db66b78f134619bed2fb
iaw47941842 <iaw47941842@h17.informatica.escoladeltreball.org>
2017-09-18 17:51:21 +0200

    First commit

---
+++ src/org/escoladeltreball/helloworld/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.escoladeltreball.helloworld;
+
+/**
+ * @author iaw47941842
+ *
+ */
+public final class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+	}
+
+}
